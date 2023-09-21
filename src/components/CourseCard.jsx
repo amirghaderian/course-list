d86c3f91ee2bceb9c2c7cc1878b0c4e756162ce1
asd <asd.it@gmail.com>
2023-09-21 21:07:07 +0330

    Destructure props & Separating the courseCard component

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,40 +1,54 @@
-const CourseCard = (props) => {
+const CourseCard = ({ course }) => {
   return (
     <div className="course-item">
       <div className="course-item__img">
-        <img src={props.course.imageUrl} alt="React course" />
+        <img src={course.imageUrl} alt="React course" />
       </div>
       <div className="course-item__detail">
-        <div className="course-item__body">
-          <div>
-            <p className="title">{props.course.title} </p>
-            <p className="desc">{props.course.description}</p>
-          </div>
-          <span className="rate">{props.course.rate}</span>
-        </div>
-        <div className="course-item__footer">
-          <div className="tags">
-            {props.course.tags.map((tag) => (
-              <span key={tag} className="badge--secondary">
-                {tag}
-              </span>
-            ))}
-          </div>
+        <CourseCardBody
+          title={course.title}
+          description={course.description}
+          rate={course.rate}
+        />
+<CourseCardFooter course={course} />
 
-          <div className="caption">
-            <div className="date ">
-              {new Date(props.course.start).toLocaleDateString("en-US", {
-                month: "short",
-                year: "numeric",
-                day: "numeric",
-              })}
-            </div>
-            <span className="badge badge--primary">{props.course.status}</span>
-          </div>
-        </div>
       </div>
     </div>
   );
 };
 
 export default CourseCard;
+
+const CourseCardBody = ({ title, description, rate }) => {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{title} </p>
+        <p className="desc">{description}</p>
+      </div>
+      <span className="rate">{rate}</span>
+    </div>
+  );
+};
+const CourseCardFooter = ({course}) => {
+   return <div className="course-item__footer">
+  <div className="tags">
+    {course.tags.map((tag) => (
+      <span key={tag} className="badge--secondary">
+        {tag}
+      </span>
+    ))}
+  </div>
+
+  <div className="caption">
+    <div className="date ">
+      {new Date(course.start).toLocaleDateString("en-US", {
+        month: "short",
+        year: "numeric",
+        day: "numeric",
+      })}
+    </div>
+    <span className="badge badge--primary">{course.status}</span>
+  </div>
+</div>
+};
